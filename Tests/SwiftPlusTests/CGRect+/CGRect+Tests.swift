25b9b9963a21472cbd702fdcc9b5996b2c6c3a06
Michael Berger <michael@bergerbytes.io>
2022-08-06 14:24:58 -0500

    Add LICENSE

--- Tests/SwiftPlusTests/CGRect+/CGRect+Tests.swift
+++ Tests/SwiftPlusTests/CGRect+/CGRect+Tests.swift
@@ -1,3 +1,17 @@
+//  Copyright © 2022 BergerBytes LLC. All rights reserved.
+//
+//  Permission to use, copy, modify, and/or distribute this software for any
+//  purpose with or without fee is hereby granted, provided that the above
+//  copyright notice and this permission notice appear in all copies.
+//
+//  THE SOFTWARE IS PROVIDED  AS IS AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+//  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+//  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+//  SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+//  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+//  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+//  IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 import Foundation
 import Nimble
 import Quick
@@ -103,6 +117,24 @@ final class CGRectTests: QuickSpec {
                         let before = rect
                         rect = rect.with(centerX: nil, centerY: nil)
                         expect(rect).to(equal(before))
+                    }
+                }
+            }
+            
+            describe("Measurements") {
+                describe("Diagonal") {
+                    it("can get diagonal") {
+                        let rect = CGRect(origin: .zero, size: .init(width: 2, height: 4))
+                        
+                        expect(rect.diagonal).to(equal(4.47213595499958))
+                        expect(rect.diagonal).to(equal(hypot(2, 4)))
+                    }
+                    
+                    it("can get diagonalSquared") {
+                        let rect = CGRect(origin: .zero, size: .init(width: 4, height: 8))
+
+                        expect(rect.diagonalSquared).to(equal(80.0))
+                        expect(rect.diagonalSquared).to(beCloseTo(pow(hypot(4, 8), 2)))
                     }
                 }
             }
