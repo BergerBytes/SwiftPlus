ca9237a1e1421500517c6eb00f4d25a83fd372ae
Michael Berger <michael@bergerbytes.io>
2022-11-20 15:24:26 -0600

    Add more conditional protection to Color+

--- Sources/SwiftPlus/Extensions/SwiftUI/Color+.swift
+++ Sources/SwiftPlus/Extensions/SwiftUI/Color+.swift
@@ -12,7 +12,7 @@
 //  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
 //  IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#if canImport(SwiftUI)
+#if canImport(SwiftUI) && (canImport(UIKit) || canImport(AppKit))
     import SwiftUI
 
     #if canImport(UIKit)
