1a4005b380dd43fa9b8af7a508e2c0b4a160ac05
Michael Berger <michael@bergerbytes.io>
2023-02-10 18:51:01 -0600

    Add support for string hex colors

--- Sources/SwiftPlus/Extensions/SwiftUI/Color+.swift
+++ Sources/SwiftPlus/Extensions/SwiftUI/Color+.swift
@@ -23,16 +23,60 @@
 
     @available(iOS 13.0, macOS 10.15, watchOS 7.0, tvOS 14.0, *)
     public extension Color {
-        init(hex: UInt, alpha: Double = 1) {
-            self.init(
-                .sRGB,
-                red: Double((hex >> 16) & 0xFF) / 255,
-                green: Double((hex >> 08) & 0xFF) / 255,
-                blue: Double((hex >> 00) & 0xFF) / 255,
-                opacity: alpha
-            )
+        /**
+         Creates a new `Color` instance from a given hexadecimal value and alpha component.
+
+         - Parameters:
+            - hex: The hexadecimal value to use for the new color, in the format 0xRRGGBB.
+            - alpha: The alpha component to use for the new color, in the range 0.0 (fully transparent) to 1.0 (fully opaque). The default value is 1.0.
+            - colorSpace: The color space to use for the new color. The default value is `.sRGB`.
+
+         - Returns: A new `Color` instance with the specified color and alpha.
+         */
+        init(hex: UInt, alpha: Double = 1, colorSpace: Color.RGBColorSpace = .sRGB) {
+            // Extract the red, green, and blue components from the hexadecimal value using bitwise operations.
+            let red = Double((hex >> 16) & 0xFF) / 255
+            let green = Double((hex >> 8) & 0xFF) / 255
+            let blue = Double(hex & 0xFF) / 255
+
+            // Call the designated initializer for `Color` with the extracted components, alpha value, and color space.
+            self.init(colorSpace, red: red, green: green, blue: blue, opacity: alpha)
         }
 
+        /**
+         Creates a new `Color` instance from a given 6-character hexadecimal string, alpha component, and color space.
+
+         - Parameters:
+            - hex: The 6-character hexadecimal string to use for the new color, in the format #RRGGBB.
+            - alpha: The alpha component to use for the new color, in the range 0.0 (fully transparent) to 1.0 (fully opaque). The default value is 1.0.
+            - colorSpace: The color space to use for the new color. The default value is sRGB.
+
+         - Returns: A new `Color` instance with the specified color and alpha.
+         */
+        init(hex: String, alpha: Double = 1, colorSpace: Color.RGBColorSpace = .sRGB) {
+            // Ensure that the hex string has 6 characters and can be parsed as a UInt with radix 16.
+            guard hex.count == 7, hex.hasPrefix("#"), let hexValue = UInt(hex.dropFirst(), radix: 16) else {
+                // If the hex string is invalid, create a clear color and return it.
+                self.init(.clear)
+                return
+            }
+
+            // Extract the red, green, and blue components from the UInt value using bitwise operations.
+            let red = Double((hexValue >> 16) & 0xFF) / 255
+            let green = Double((hexValue >> 8) & 0xFF) / 255
+            let blue = Double(hexValue & 0xFF) / 255
+
+            // Call the designated initializer for `Color` with the extracted components, alpha value, and color space.
+            self.init(colorSpace, red: red, green: green, blue: blue, opacity: alpha)
+        }
+
+        /**
+         Returns the opaque hexadecimal value of the receiver as a UInt.
+         
+         This function extracts the red, green, and blue components from the receiver using the `getRed(_:green:blue:alpha:)` method of `UIColor` or `NSColor`, depending on the platform. It then multiplies each component by 255, converts the results to a UInt, and combines them into a single value using bitwise operations. The resulting value is the opaque hexadecimal representation of the color.
+         
+         - Returns: The opaque hexadecimal value of the receiver as a UInt.
+         */
         #if canImport(UIKit) || canImport(AppKit)
             @available(iOS 14.0, *)
             @available(macOS 11, *)
